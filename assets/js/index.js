458de706054c3512a9950aa9754a8e97f0eb793d
unknown <dudeomous@gmail.com>
2023-05-28 20:32:33 -0400

    added some css and some consts for dom manipulation

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+//variables
+const apikey = '03aa7027444e2d3af9f29696b97c7af9'
+const searchForm = document.getElementById('search-form') 
+const searchInput = document.getElementById('search-input') 
+const = document.getElementById('') 
+const = document.getElementById('') 
